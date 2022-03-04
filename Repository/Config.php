684c1610db219690b4f579ec48feedf6b8e7890a
sajjadRabiee <71874588+sajjadRabiee@users.noreply.github.com>
2022-03-04 23:28:25 +0330

    Update Config.php

--- Repository/Config.php
+++ Repository/Config.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_HOST','localhost');
 define('DB_USER','root');
-define('DB_PASS','8391218227');
+define('DB_PASS','password');
 define('DB_NAME','crud');
 session_start();
 $db = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
